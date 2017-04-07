c82ff68c8cc296c3ca3225327abe206b4aebdef4
Alex Yuan <alexyuan@wireless-guest-121.nat.emory.edu>
2017-04-07 19:52:50 -0400

    json and npm packages

---
+++ liri.js
@@ -0,0 +1,8 @@
+//my-tweets - shows last 20 tweets when they are created at in your terminal/bash
+//spotify-this-song '<song name here>' - shows artist, song name, preview link, album. if no song provided, default will be "The Sign by Ace of Base"
+//movie-this '<movie name here>' - outputs title of movie, year of movie, IMDB rating, Country of production, Language of movie, plot, actors, Rotten Tomatoes rating, if no movie input, default movie is "Mr. Nobody"
+//do-what-it-says runs the random.txt file
+//bonus: create file call log.txt, append each command you run in to log.txt
+
+var inputCommand = process.argv[2];
+console.log(inputCommand);
